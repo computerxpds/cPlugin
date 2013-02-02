b20c9da7a05190c85aa7eb9c3a67bdaa13e8690b
computerxpds <dstrickner@gmail.com>
2013-02-02 15:37:59 -0500

    Added proper startup console output to show its been loaded.

--- src/me/comp/cPlugin/Main.java
+++ src/me/comp/cPlugin/Main.java
@@ -1,10 +1,14 @@
 package me.comp.cPlugin;
 
+import java.util.logging.Logger;
+
 import mc.comp.cPlugin.commands.Foo;
 
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+	   public final Logger logger = Logger.getLogger("Minecraft");
 	
 	
 	@Override
@@ -19,5 +23,7 @@ public class Main extends JavaPlugin {
 		
 		getServer().getPluginManager().registerEvents(new PlayerListener(), this);
 		this.getCommand("foo").setExecutor(new Foo());
+		PluginDescriptionFile pdffile = this.getDescription();
+		this.logger.info(pdffile.getName() + "is now enabled.");
 	}
 }
