446d87ca571058d24ecd43b2041e7050748b3775
computerxpds <dstrickner@gmail.com>
2013-02-18 11:55:07 -0500

    This should work better now..

--- src/me/comp/cPlugin/PlayerListener.java
+++ src/me/comp/cPlugin/PlayerListener.java
@@ -9,7 +9,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-public class PlayerListener implements Listener {
+public class PlayerListener extends Main implements Listener {
 	Main plugin;
 	
 	public PlayerListener(Main instance){
